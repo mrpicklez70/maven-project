7b2026b78efac594114928286951e00edce50ba0
mrpicklez70 <varosb.16@gmail.com>
2019-05-10 10:24:50 -0600

    more stuff

--- src/main/java/resources/param/BasicClass.java
+++ src/main/java/resources/param/BasicClass.java
@@ -5,5 +5,18 @@ public class BasicClass {
 	public static final String RESOURCE = "https://qa8-app.snapfinance.com";
 	public static final String USER = "user";
 	public static final String PASSWORD = "pass|";
+	
+	
+	/*
+	 * asdfasldfjsaf
+	 * asdfasdf
+	 * asdfasdfasdf
+	 * asdfasdfasdf
+	 * asdfasdfasdf
+	 * asdfasdfadsf
+	 * asdfasdfas
+	 * dafdsasdfasdf
+	 */
+	
 
 }
